97cc8ef05762021491183c956f732319c888f4ec
jjones-nbisd <jjones@nbschools.net>
2026-06-23 21:47:14 -0500

    Update meraki.js

--- meraki.js
+++ meraki.js
@@ -1,56 +1,46 @@
-// Path: meraki.js
-
-// Helper function to parse URL
-function GetURLParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
+function GetURLParameter(sParam) {
+  const params = new URLSearchParams(window.location.search);
+  return params.get(sParam);
 }
 
-// Parse URL parameters for Meraki appended items
-var base_grant_url = decodeURIComponent(GetURLParameter("base_grant_url"));
-var user_continue_url = decodeURIComponent(GetURLParameter("user_continue_url"));
-var node_mac = GetURLParameter("node_mac");
-var client_ip = GetURLParameter("client_ip");
-var client_mac = GetURLParameter("client_mac");
+const base_grant_url = decodeURIComponent(GetURLParameter("base_grant_url") || "");
+const user_continue_url = decodeURIComponent(GetURLParameter("user_continue_url") || "");
+const node_mac = GetURLParameter("node_mac") || "";
+const client_ip = GetURLParameter("client_ip") || "";
+const client_mac = GetURLParameter("client_mac") || "";
 
-// Output Meraki API parameters to console for debugging
-console.log("base_grant_url: " + base_grant_url);
-console.log("user_continue_url: " + user_continue_url);
-console.log("node_mac: " + node_mac);
-console.log("client_ip: " + client_ip);
-console.log("client_mac: " + client_mac);
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwrnJD5DU7gCVDD6S0fYZi6Evj7W2pxNf6Y43zfJFOgHEV2IDHQ5dgCEbQr6tri1IjaFw/exec";
 
-// Function to redirect user to Meraki auth URL
-function authUser(){
-  var loginUrl = base_grant_url;
+function authUser() {
+  let loginUrl = base_grant_url;
 
-  // Check if user continue URL is defined
-  if(user_continue_url !== "undefined")
-      loginUrl += "?continue_url=" + user_continue_url;
-  
-  // Redirect user to Meraki auth URL
+  if (user_continue_url && user_continue_url !== "undefined") {
+    loginUrl += "?continue_url=" + encodeURIComponent(user_continue_url);
+  }
+
   window.location.href = loginUrl;
 }
 
-// Function to store user entered data and start auth
-function login(){
+function login() {
+  const data = {
+    timestamp: new Date().toISOString(),
+    node_mac,
+    client_ip,
+    client_mac,
+    user_continue_url,
+    base_grant_url
+  };
 
-  // **************************
-  // store user data to db here
-  // **************************
-
-  // Start user auth to Meraki
-  authUser();
+  fetch(GOOGLE_SCRIPT_URL, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }).finally(() => {
+    authUser();
+  });
 }
 
-// Add click listener to continue button to login user
-document.getElementById('continue-btn').onclick = function() {login()}
+document.getElementById("continue-btn").onclick = login;
